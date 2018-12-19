40f90331dc260e13197e95103ce7e48deb8301ac
Martin Lukas <martinlukas456@gmail.com>
2018-12-19 17:38:58 +0200

    Updates

--- precache-manifest.65ca1a570c965460b00b844f71f6ec04.js
+++ precache-manifest.65ca1a570c965460b00b844f71f6ec04.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/2018-interactive-web-apps-project-martin-lukas/static/js/runtime~main.5d057912.js"
   },
   {
-    "revision": "51358b493d5813e91eef",
-    "url": "/2018-interactive-web-apps-project-martin-lukas/static/js/main.51358b49.chunk.js"
+    "revision": "f7d7e8cf15b3ecec0143",
+    "url": "/2018-interactive-web-apps-project-martin-lukas/static/js/main.f7d7e8cf.chunk.js"
   },
   {
     "revision": "e0f7769fcf4385d0caa2",
     "url": "/2018-interactive-web-apps-project-martin-lukas/static/js/1.e0f7769f.chunk.js"
   },
   {
-    "revision": "51358b493d5813e91eef",
-    "url": "/2018-interactive-web-apps-project-martin-lukas/static/css/main.66280157.chunk.css"
+    "revision": "f7d7e8cf15b3ecec0143",
+    "url": "/2018-interactive-web-apps-project-martin-lukas/static/css/main.5b1b2ce9.chunk.css"
   },
   {
-    "revision": "1684d4872ab871b4f1f0c41774f0ae6a",
+    "revision": "418c9e0e8a8eb85fa76a3ce8cf9a2955",
     "url": "/2018-interactive-web-apps-project-martin-lukas/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/2018-interactive-web-apps-project-martin-lukas/precache-manifest.a3ff5b1b79a31e9d7833817562c647b5.js"
+  "/2018-interactive-web-apps-project-martin-lukas/precache-manifest.65ca1a570c965460b00b844f71f6ec04.js"
 );
 
 workbox.clientsClaim();
