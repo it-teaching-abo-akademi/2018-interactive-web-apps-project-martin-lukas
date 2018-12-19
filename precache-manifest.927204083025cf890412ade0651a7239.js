79a13c0321414bf9fe9b59ed4e95b8565d8d6fbd
Martin Lukas <martinlukas456@gmail.com>
2018-12-19 15:44:23 +0200

    Updates

---
+++ precache-manifest.927204083025cf890412ade0651a7239.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d057912239dfe648b1c",
+    "url": "/2018-interactive-web-apps-project-martin-lukas/static/js/runtime~main.5d057912.js"
+  },
+  {
+    "revision": "216f2e6bca7d884816ae",
+    "url": "/2018-interactive-web-apps-project-martin-lukas/static/js/main.216f2e6b.chunk.js"
+  },
+  {
+    "revision": "dfaee5d138d59d899876",
+    "url": "/2018-interactive-web-apps-project-martin-lukas/static/js/1.dfaee5d1.chunk.js"
+  },
+  {
+    "revision": "216f2e6bca7d884816ae",
+    "url": "/2018-interactive-web-apps-project-martin-lukas/static/css/main.66280157.chunk.css"
+  },
+  {
+    "revision": "9bf0f775c496841241745f45a1bf23c6",
+    "url": "/2018-interactive-web-apps-project-martin-lukas/index.html"
+  }
+];
